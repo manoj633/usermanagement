fc61f1c0d15beda060b87356fd483406d3f04963
Manoj <manoj09633@gmail.com>
2022-10-05 21:18:18 +0530

    Components created for adding and viewing users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ViewUserComponent
   ],
   imports: [
     BrowserModule,
